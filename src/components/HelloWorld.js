d088c2522c23a759516fc228e172b4b16fd54c0d
sidoncode <siddharth54330@gmail.com>
2023-02-22 10:02:10 +0530

    all commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+class HelloWorld extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+                <h1>Hello World</h1>
+                <p>Hello HI and How are you?</p>    
+                
+            </div>
+        )
+    }
+
+
+
+}
+
+export default HelloWorld;
